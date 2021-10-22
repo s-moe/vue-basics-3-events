16a50e1a0cbedfcdb3d74a2b03848bbafc5ac94a
s-moe <sarahannmoe@gmail.com>
2021-10-22 14:32:43 -0700

    watchers vs computed vs methods

--- app.js
+++ app.js
@@ -3,28 +3,44 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
       confirmedName: "",
-      fullName: "",
+      // fullName: "",
     };
   },
+  //can watch any property with them, or run something in reaction (non-data updates to make). Used the least.
   watch: {
-    name(value) {
-      if (value === "") {
-        this.fullName = "";
-      } else {
-        this.fullName = value + " " + "Moe";
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
       }
     },
-  },
-  computed: {
-    // fullName() {
-    //   console.log("hi");
-    //   if (this.name === "") {
-    //     return "";
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = value + " " + this.lastName;
     //   }
-    //   return this.name + " " + "Moe";
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = this.name + " " + value;
+    //   }
     // },
   },
+  //use computed for data binding - computed properties are only re-evaluated if one of their "used values" changed (not if other data changes)
+  computed: {
+    fullName() {
+      console.log("hi");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
+  },
+  //use methods to bind data, event binding - for data it will be executed for every re-render, so it should only be used for event binding or data binding IF YOU WNAT IT TO RE-RENDER
   methods: {
     // outputFullName() {
     //   if (this.name === "") {
