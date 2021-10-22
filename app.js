425fba37aea07eb4166ad610202cb74387def7a1
s-moe <sarahannmoe@gmail.com>
2021-10-22 10:16:28 -0700

    added

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const app = Vue.createApp({
     subtractOne(num) {
       this.counter = this.counter - num;
     },
-    setName(event) {
-      this.name = event.target.value;
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
     },
   },
 });
