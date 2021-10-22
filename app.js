1b1afbdf3f6da0d25dc3a2f9cfb3ebd4f3a497e8
s-moe <sarahannmoe@gmail.com>
2021-10-22 09:57:40 -0700

    tidying up html by using methods

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    addOne() {
+      return this.counter++;
+    },
+    subtractOne() {
+      return this.counter--;
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
